92ab11cd937915940bcef1fbbc16d748366208fe
generator-csp <CHETNAP1999@GMAIL.COM>
2025-08-24 07:59:47 +0530

    Update subscriber.js

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
+
+const subscriberSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+module.exports = mongoose.model("Subscriber", subscriberSchema);
 
